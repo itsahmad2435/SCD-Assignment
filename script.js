9ab775ce9c59bba22257737088b28a652a91cef5
Ahmad Nisar <ahmadnanisar3582@gmail.com>
2026-03-08 21:38:49 +0500

    TASK-3 Implement add/delete task with delete button and styling

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function addTask() {
     let task = document.getElementById("taskInput").value;
-    if (task === "") return; // prevent empty task
+    if (task === "") return;
+
     let li = document.createElement("li");
     li.innerText = task;
 
-    // Add delete on click
-    li.onclick = function() {
-        this.remove();
+    // Create Delete button
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    delBtn.onclick = function() {
+        li.remove();
     };
 
+    li.appendChild(delBtn);
     document.getElementById("taskList").appendChild(li);
+
+    document.getElementById("taskInput").value = "";
 }
